e48bb8eb5b223e1bc6260b45f024ca5e404cab94
Ethkuil <hobartlin23@gmail.com>
2023-05-16 07:11:09 -0700

    feat(contest.lua): 曲线救国实现赛前清空rating

--- contest.lua
+++ contest.lua
@@ -1,33 +1,40 @@
-local count = 2
+period = 3600 * 6 -- 6 小时一场比赛
+
+count_max = period / 2 -- 文档里说每隔2秒触发一次 on_timer
+
+local count = period / 2 - 1 -- 为了在更新赛事脚本后立即触发 on_timer 开始比赛
 local su_id = get_id('su')
 
 function on_submission(all, from)
     print('Submission', from)
-    -- 自己和自己比赛
-    create_match(from, from)
+    -- -- 自己和自己比赛
+    -- create_match(from, from)
 end
 
 function on_timer(all)
     count = count + 1
     -- 不知道为什么，这里插入了语句就不能正常运行
-    if count < 3600 * 2 then return end
+    if count < count_max then
+        return
+    end
     count = 0
     print('Timer')
     print('Superuser has ID ' .. tostring(su_id))
     print('Creating matches for contest #3')
     print('Number of participants with delegates ' .. tostring(#all))
 
+    -- 先跑一圈自己和自己比赛，曲线救国清空rating
     for i = 1, #all do
-        all[i].rating = 0
+        if all[i].rating ~= 0 then
+            create_match(all[i].id, all[i].id) 
+        end
     end
 
-    -- 单循环赛
+    -- 单循环赛. 不得含自己和自己比赛
     for i = 1, #all do
         for j = i + 1, #all do
             create_match(all[i].id, all[j].id)
         end
-        print(string.format('Contestant %s (%d), rating %d, performance "%s"',
-            all[i].handle, all[i].id, all[i].rating, all[i].performance))
     end
 end
 
@@ -36,6 +43,13 @@ function on_manual(all, arg)
 end
 
 function update_stats(report, par)
+    -- 自己和自己比赛时，清空rating。曲线救国
+    if par[1].id == par[2].id then
+        par[1].rating = 0
+        par[2].rating = 0
+        return
+    end
+
     -- 忽略第一个 { 前的内容
     local report_start_idx = report:find("{")
     if not report_start_idx then return end
